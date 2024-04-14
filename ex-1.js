30e84efb75f5d8a34ed68fbb0a423ff777a28ae8
black3nz <pawich.p@hotmail.com>
2024-04-14 14:13:14 +0700

    Assignment submission

--- ex-1.js
+++ ex-1.js
@@ -1,4 +1,12 @@
 // Exercise #1: Decrease the Student Scores
 let scores = [89, 70, 60, 50, 100, 71];
 
-// Start coding here
+function decrease10(inputScoresArray){
+    let newArray = [];
+    for (let i = 0; i < inputScoresArray.length; i++){
+        newArray.push(inputScoresArray[i] - 10);
+    }
+    return newArray
+}
+let result = decrease10(scores);
+console.log(result);
--- ex-6.js
+++ ex-6.js
@@ -1,7 +1,15 @@
 // Exercise #6: Reverse the String
 let companyName = "TechUp Thailand";
-let reversedCompanyName = "";
+let reversedCompanyName;
 
 // Start coding here
-
+function reversedStr(str){
+    let reversed = "";
+    for (let i = str.length - 1; i >= 0; i--){
+        reversed += str[i];
+    }
+    console.log(reversed);
+    return reversed;
+}
+reversedCompanyName = reversedStr(companyName);
 console.log(reversedCompanyName);
